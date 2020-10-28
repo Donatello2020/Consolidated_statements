6ac87fc65bcc779be540990f1dad0938bd2418c5
Donatello2020 <635155550@qq.com>
2020-10-28 17:00:36 +0800

    2020年10月28日 17:00:31

--- new_data/new_data.py
+++ new_data/new_data.py
@@ -1,3 +1,19 @@
 import os
+import sys
 import xlwings as xw
-import configparser
+
+# import configparser
+curpath = os.path.dirname(os.path.realpath(__file__))
+xlspath = os.path.join(curpath, 'xlsbook')
+xls_name = []
+for _xls_name in os.listdir(xlspath):
+    if _xls_name.endswith('xlsx'):
+        xls_name.append(_xls_name)
+# print(xls_name)
+if not xls_name:
+    print('没有找到Excel表格,请检查当前目录')
+    sys.exit(0)
+for _xls_name in xls_name:
+    wb = xw.Book(os.path.join(xlspath, _xls_name))
+    wb.api.Application.ErrorCheckingOptions.BackgroundChecking = False  # 关闭Excel错误检查
+    break
